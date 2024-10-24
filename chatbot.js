44dda7e068c6a1ae5d3ad6046c4b678d0b53b90e
Ami Ouedraogo <AOuedraogo@intershop.com>
2024-10-24 13:19:14 +0200

    first commit

---
+++ chatbot.js
@@ -0,0 +1,74 @@
+import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+Chatbot.init({
+  chatflowid: "bf4d560b-9691-48e4-85ac-7e87d85d2500",
+  apiHost: "http://localhost:3000",
+  chatflowConfig: {
+    vars: {
+      currentUrl: window.location.href,
+      topic: "cec"
+    }
+  },
+  observersConfig: {},
+  theme: {
+    button: {
+      backgroundColor: '#008e87',
+      right: 20,
+      bottom: 20,
+      size: 48, // small | medium | large | number
+      dragAndDrop: true,
+      iconColor: 'white',
+      autoWindowOpen: {
+        autoOpen: true, //parameter to control automatic window opening
+        openDelay: 2, // Optional parameter for delay time in seconds
+        autoOpenOnMobile: false, //parameter to control automatic window opening in mobile
+      },
+    },
+    chatWindow: {
+      showTitle: true,
+      showAgentMessages: true,
+      title: 'Intershop Documentation Chatbot',
+      titleAvatarSrc: "message.png",
+      welcomeMessage: 'Hello! Welcome to Intershop Documentation ChatBot',
+      errorMessage: 'This is a custom error message',
+      backgroundColor: '#ffffff',
+      height: 700,
+      width: 400,
+      fontSize: 16,
+      clearChatOnReload: false,
+      sourceDocsTitle: 'Sources:',
+      botMessage: {
+        backgroundColor: '#f7f8ff',
+        textColor: '#303235',
+        showAvatar: true,
+        avatarSrc: 'https://github.com/amisamyra99/FlowiseChatEmbed/blob/main/src/assets/images/message.png',
+      },
+      userMessage: {
+        backgroundColor: '#008e87',
+        textColor: '#ffffff',
+        showAvatar: true,
+        avatarSrc: 'https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png',
+      },
+      textInput: {
+        placeholder: 'Type your question',
+        backgroundColor: '#ffffff',
+        textColor: '#303235',
+        sendButtonColor: '#008e87',
+        maxChars: 50,
+        maxCharsWarningMessage: 'You exceeded the characters limit. Please input less than 50 characters.',
+        autoFocus: true,
+        sendMessageSound: true,
+        receiveMessageSound: true,
+      },
+      footer: {
+        textColor: '#303235',
+        text: 'Powered by',
+        company: 'Flowise',
+        companyLink: 'https://flowiseai.com',
+      },
+      disclaimer: {
+        title: 'Disclaimer',
+        message: 'By using this chatbot, you agree to the <a target="_blank" href="https://flowiseai.com/terms">Terms & Condition</a>',
+      }
+    },
+  }
+});
